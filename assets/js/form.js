e5299311174f024d3290206dc3eb060676c56982
Federico <fs.rodriguez91@gmail.com>
2020-07-10 18:49:27 -0300

    firebase functions mailing

--- assets/js/form.js
+++ assets/js/form.js
@@ -2,6 +2,11 @@ const db = firebase.firestore();
 const form = document.querySelector('#confirmation-form');
 const button = document.getElementById("confirm-btn");
 
+// Firebase Auth
+firebase.auth().signInAnonymously().catch(function (error) {
+    console.log('error :>> ', `(${error.code}): ${error.message}`);
+});
+
 // Save guest confirmation
 
 form.addEventListener('submit', function (e) {
@@ -11,9 +16,9 @@ form.addEventListener('submit', function (e) {
     const songElements = document.querySelectorAll('.song');
     let songs = [];
     for (let i = 0; i < songElements.length; i++) {
-        const artist = form.artist-[i].value;
-        const song = form.song-[i].value;
-        
+        const artist = form.artist - [i].value;
+        const song = form.song - [i].value;
+
         if (!!artist && !!song) {
             songs.push({
                 artist,
@@ -23,30 +28,29 @@ form.addEventListener('submit', function (e) {
     }
 
     db.collection('guests').add({
-        name: form.name.value,
-        lastname: form.lastname.value,
-        adults: form.adt.value,
-        children: form.chd.value,
-        veggie: form.veggie.value,
-        celiaco: form.celiaco.value,
-        songs,
-        comments: form.comments.value ? form.comments.value : 'Sin comentarios'
-    })
-    .then(function() {
-        let text = "Los esperamos"
-        if (form.adt.value == 1 && form.chd.value == 0) {
-            text = "Te esperamos"
-        }
-        swal("Muchas gracias!", text, "success");
-        button.style.backgroundColor="#28a745"
-        button.style.borderColor="#28a745"
-        button.textContent = "Gracias por confirmar!"
-        form.reset();
-    })
-    .catch(function(error) {
-        swal("Lo sentimos", "Hubo un error al intentar enviar tu confirmación", "error");
-        button.disabled = false;
-        console.error("Error adding document: ", error);
-    });
+            name: form.name.value,
+            lastname: form.lastname.value,
+            adults: form.adt.value,
+            children: form.chd.value,
+            veggie: form.veggie.value,
+            celiaco: form.celiaco.value,
+            songs,
+            comments: form.comments.value ? form.comments.value : 'Sin comentarios'
+        })
+        .then(function () {
+            let text = "Los esperamos"
+            if (form.adt.value == 1 && form.chd.value == 0) {
+                text = "Te esperamos"
+            }
+            swal("Muchas gracias!", text, "success");
+            button.style.backgroundColor = "#28a745"
+            button.style.borderColor = "#28a745"
+            button.textContent = "Gracias por confirmar!"
+            form.reset();
+        })
+        .catch(function (error) {
+            swal("Lo sentimos", "Hubo un error al intentar enviar tu confirmación", "error");
+            button.disabled = false;
+            console.error("Error adding document: ", error);
+        });
 });
-
--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,84 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const nodemailer = require('nodemailer');
+const hbs = require('nodemailer-express-handlebars');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+const db = admin.firestore();
+
+const EMAIL = functions.config().auth.user;
+const PASSWORD = functions.config().auth.pass;
+
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: EMAIL,
+        pass: PASSWORD
+    }
+});
+
+
+// Emails the client when a new guest confirms
+exports.newGuest = functions.firestore.document('guests/{guestId}').onCreate(async (change, context) => {
+
+    // Read the guest document
+    const guestSnap = await db.collection('guests').doc(context.params.guestId).get();
+
+    // Raw Data
+    const guest = guestSnap.data();
+
+    console.log('guest :>> ', guest);
+
+    // Fields required validation
+    const isValid = guest.name && guest.lastname && guest.comments &&
+        guest.adults !== null && guest.children !== null;
+
+    if (!isValid) {
+        return {
+            message: 'Invitado no válido'
+        }
+    }
+
+    // Template
+    transporter.use('compile', hbs({
+        viewEngine: {
+            extName: '.hbs',
+            partialsDir: 'templates',
+            layoutsDir: 'templates',
+            defaultLayout: 'confirmation.hbs',
+        },
+        viewPath: 'templates',
+        extName: '.hbs'
+    }));
+
+
+    // Email
+    const mailOptions = {
+        from: EMAIL,
+        to: 'fs.rodriguez91@gmail.com, alfonzodenise@gmail.com',
+        subject: `Fede & Denu - ${guest.name} ${guest.lastname} x${guest.adults}`,
+        template: 'confirmation',
+        context: {
+            name: guest.name,
+            lastname: guest.lastname,
+            adults: guest.adults,
+            children: guest.children,
+            veggie: guest.veggie ? guest.veggie : 0,
+            celiaco: guest.celiaco ? guest.celiaco : 0,
+            songs: guest.songs,
+            comments: guest.comments
+        }
+    };
+
+    // Send it
+    return transporter.sendMail(mailOptions, (err, data) => {
+        if (err) {
+            return err.message;
+        }
+
+        return {
+            message: `Mensaje enviado a ${mailOptions.to}`
+        };
+    });
+
+});
