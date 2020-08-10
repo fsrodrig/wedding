1abcc23a6283115fb6c80e7e3a1376544d6fe921
Federico <fs.rodriguez91@gmail.com>
2020-08-10 20:27:14 -0300

    change church time

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,38 +1,43 @@
 // HEART ANIMATION
-let bg = document.querySelector('#bg');
-window.addEventListener('scroll', function () {
-    let value = window.scrollY;
-    bg.style.backgroundSize = 3 * value + 'px';
+let bg = document.querySelector("#bg");
+window.addEventListener("scroll", function () {
+  let value = window.scrollY;
+  bg.style.backgroundSize = 3 * value + "px";
 });
 
 // JQuery Functions
-! function (a) {
+!(function (a) {
+  a(window).bind("scroll", function () {
+    if (a(window).scrollTop() > 30) {
+      a(".down").hide();
+    } else {
+      a(".down").show();
+    }
+  });
 
-    a(window).bind('scroll', function() {
-        if (a(window).scrollTop() > 30) {
-            a('.down').hide();
-        }
-        else {
-            a('.down').show();
-        }
-   });
-
-    // COUNTDOWN
-    a(".countdown").countdown("2021/02/12 17:00:00").on("update.countdown", function (s) {
-        a(this).html(s.strftime('<div class="col"><div class="card card-body countdown-shadow mb-4 mb-lg-0 p-3"><span class="counter mb-1 ">%D</span> <span class="label">Día%!D</span></div></div> <div class="col"><div class="card card-body countdown-shadow mb-4 mb-lg-0 p-3"><span class="counter mb-1">%H</span> <span class="label">Hora%!H</span></div></div> <div class="col"><div class="card card-body countdown-shadow mb-4 mb-lg-0 p-3"><span class="counter mb-1">%M</span> <span class="label">Minuto%!M</span></div></div> <div class="col"><div class="card card-body countdown-shadow p-3"><span class="counter mb-1">%S</span> <span class="label">Segundo%!S</span></div></div>'))
+  // COUNTDOWN
+  a(".countdown")
+    .countdown("2021/02/12 17:45:00")
+    .on("update.countdown", function (s) {
+      a(this).html(
+        s.strftime(
+          '<div class="col"><div class="card card-body countdown-shadow mb-4 mb-lg-0 p-3"><span class="counter mb-1 ">%D</span> <span class="label">Día%!D</span></div></div> <div class="col"><div class="card card-body countdown-shadow mb-4 mb-lg-0 p-3"><span class="counter mb-1">%H</span> <span class="label">Hora%!H</span></div></div> <div class="col"><div class="card card-body countdown-shadow mb-4 mb-lg-0 p-3"><span class="counter mb-1">%M</span> <span class="label">Minuto%!M</span></div></div> <div class="col"><div class="card card-body countdown-shadow p-3"><span class="counter mb-1">%S</span> <span class="label">Segundo%!S</span></div></div>'
+        )
+      );
     });
 
-    // ADD SONGS
-    const maxSongs = 4;
-    let cantSongs = 0;
-    a("#addSong").click(function (e) {
-        e.preventDefault();
-        if (cantSongs < maxSongs) {
-            if (cantSongs === 3) {
-                a('#addSong').attr("disabled", true);
-            }
+  // ADD SONGS
+  const maxSongs = 4;
+  let cantSongs = 0;
+  a("#addSong").click(function (e) {
+    e.preventDefault();
+    if (cantSongs < maxSongs) {
+      if (cantSongs === 3) {
+        a("#addSong").attr("disabled", true);
+      }
 
-            a('#songs').append(`<div id="added-${cantSongs}" class="row song" style="display: none;">
+      a("#songs")
+        .append(`<div id="added-${cantSongs}" class="row song" style="display: none;">
         <div class="col-md-6">
            <div class="form-group">
               <label>Artista</label>
@@ -47,8 +52,8 @@ window.addEventListener('scroll', function () {
            </div>
         </div>
      </div>`);
-            a(`#added-${cantSongs}`).slideDown('slow');
-            cantSongs++;
-        }
-    });
-}(jQuery);
+      a(`#added-${cantSongs}`).slideDown("slow");
+      cantSongs++;
+    }
+  });
+})(jQuery);
